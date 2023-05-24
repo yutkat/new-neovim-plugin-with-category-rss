173737ba5156d8556ae3b6cb9fd1d8949e4c4f25
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-05-25 06:48:15 +0900

    Fix CI

--- scripts/get_updated_repo.sh
+++ scripts/get_updated_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-list=$(cd my-neovim-pluginlist && git diff --minimal -u --unified=0 $(git log --grep="Add" --no-merges --oneline -1 --pretty=%H)\^\! [a-z]*.md | grep github.com | grep "^+" | grep -oP '\(https://github.com/[^)]+\)' | tr -d '()')
+list=$(cd my-neovim-pluginlist && git diff --minimal -u --unified=0 $(git log --grep="Add" --no-merges --oneline -1 --pretty=%H)^\! [a-z]*.md | grep github.com | grep "^+" | grep -oP '\(https://github.com/[^)]+\)' | tr -d '()')
 
 for l in $list; do
 	r=$(echo $l | sed 's#https://github.com/##g')
